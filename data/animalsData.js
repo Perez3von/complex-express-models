249b995a702a20b4e2abb36ca0559b87dc51edec
Evon Perez <perez.evon@gmail.com>
2021-10-04 17:15:45 -0400

    adding animals, species

---
+++ data/animalsData.js
@@ -0,0 +1,54 @@
+export const animals = [
+
+
+  {
+    name: 'T-rex',
+    species_id: 2
+
+  },
+
+
+  {
+    name: 'Anaconda',
+    species_id: 3
+
+  },
+
+
+  {
+    name: 'Dog',
+    species_id: 5
+
+  },
+
+
+  {
+    name: 'Grreat white shark',
+    species_id: 1
+
+  },
+
+
+  {
+    name: 'Megladon',
+    species_id: 1
+
+  },
+
+  {
+    name: 'Penguin',
+    species_id: 4
+  },
+
+
+  {
+    name: 'Human',
+    species_id: 5
+
+  },
+
+
+
+];
+
+module.exports = animals; 
